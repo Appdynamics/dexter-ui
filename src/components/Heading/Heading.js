eb0170be3aff11c1c7ba3b07ed9f0bc6a05512f7
Daniel Arrizza <daniel.arrizza@appdynamics.com>
2018-07-01 15:48:24 -0400

    fix navbar breakpoint

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -4,21 +4,11 @@ import { Link } from 'react-router-dom'
 class Heading extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
         <Link className="navbar-brand" to="/">
           DEXTER-UI
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+
         <Link to="/input">
           <button className="btn btn-primary my-2 my-sm-0">
             Create Job File
