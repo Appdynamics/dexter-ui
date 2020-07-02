adb0619f75bffcd3b5c0e3cebed22d653c18367c
Daniel Odievich <danielodievich@hotmail.com>
2020-07-01 19:47:16 -0700

    Updates

---
+++ precache-manifest.253c1ce29015a8276fccfb16577e3ef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48789f750819f203385781d519235db2",
+    "url": "/dexter-ui/index.html"
+  },
+  {
+    "revision": "36ac9d33d42da20430de",
+    "url": "/dexter-ui/static/css/main.61d41ae0.chunk.css"
+  },
+  {
+    "revision": "578c7b35aea6d7e1d861",
+    "url": "/dexter-ui/static/js/2.b61f080a.chunk.js"
+  },
+  {
+    "revision": "74f93d9d81d0335c27759e6c8225157f",
+    "url": "/dexter-ui/static/js/2.b61f080a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36ac9d33d42da20430de",
+    "url": "/dexter-ui/static/js/main.94f8e782.chunk.js"
+  },
+  {
+    "revision": "7112a0cb0a11f80f76fd",
+    "url": "/dexter-ui/static/js/runtime-main.12b6a19f.js"
+  }
+]);
